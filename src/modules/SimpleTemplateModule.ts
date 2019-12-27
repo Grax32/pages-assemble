386e4401e1ff33c746ea9081da7024255fc8b152
David Walker <dwalker@vorteon.com>
2019-12-26 21:24:53 -0500

    update template

--- src/modules/SimpleTemplateModule.ts
+++ src/modules/SimpleTemplateModule.ts
@@ -9,16 +9,23 @@ export default class SimpleTemplateModule implements IBuildModule {
   public next!: (context: BuildContext) => ResultContext;
   public invoke(context: BuildContext): ResultContext {
     if (context.options.verbose) {
-      console.log('SimpleTemplateModule');
+      console.log('Entering SimpleTemplateModule');
     }
 
     context.assets
       .filter(v => v.outputType === OutputType.html)
       .forEach(asset => {
         const outputTemplate = '<html><body>' + asset.sections['main'] + '</body></html>';
-        const outputFolder = path.join(context.options.output, path.dirname(asset.path));
-        const outputPath = path.join(context.options.output, asset.path);
-        console.log(process.cwd(), outputPath);
+        const link = <string>(asset.frontMatter.permalink || asset.path.replace(/\.md$/, ''));
+        let outputPath = path.join(context.options.output, link);
+
+        if (!outputPath.endsWith('.html')) {
+          outputPath += '.html';
+        }
+
+        outputPath = outputPath.replace(/\\/g, '/');
+
+        const outputFolder = path.dirname(outputPath);
 
         fs.mkdirSync(outputFolder, { recursive: true });
         fs.writeFileSync(outputPath, outputTemplate);
