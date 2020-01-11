5e7f04264c5d1d781eb549cc71ae2c367654b86c
David Walker <dwalker@vorteon.com>
2020-01-11 08:15:23 -0500

    vash

--- src/modules/CompileVashRazorTemplateModule.ts
+++ src/modules/CompileVashRazorTemplateModule.ts
@@ -2,21 +2,15 @@ const Vash = require('vash');
 import 'reflect-metadata';
 import 'fs';
 import 'path';
-import { injectable } from 'inversify';
-
-import IBuildModule from '../interfaces/IBuildModule';
 import BuildContext from '../models/BuildContext';
 import ResultContext from '../models/ResultContext';
-import OutputType from '../models/OutputType';
 import BaseModule from './BaseModule';
-import ILogger from '../interfaces/ILogger';
 
 export default class CompileVashRazorTemplateModule extends BaseModule {
   public next!: (context: BuildContext) => ResultContext;
   public invoke(context: BuildContext): ResultContext {
     if (context.options.verbose) {
       console.log('Entering CompileVashRazorTemplateModule');
-      console.log(Reflect.getMetadata('injector:parameterTypes', CompileVashRazorTemplateModule));
     }
 
     context.assets
